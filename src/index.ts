4262dbdb3552f8f3137ba8b95cbfbf2a32c3304c
phamnghia <nghiapd92@gmail.com>
2025-11-24 10:39:07 +0700

    Exposed api docs as Markdown for LLMs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import userRoutes from "./modules/user/user.routes";
 import authRoutes from "./modules/auth/auth.routes";
+import { createMarkdownFromOpenApi } from "@scalar/openapi-to-markdown";
 
 const app = new OpenAPIHono();
 
@@ -21,10 +22,25 @@ app.doc("/api-specs", {
     version: "1.0.0",
     title: "Bun Hono API",
   },
+  servers: [
+    { url: "http://localhost:3000" },
+    { url: "https://bun-hono-api.vercel.app" },
+  ],
 });
 
 // Scalar API Reference
 app.get("/docs", Scalar({ url: "/api-specs" }));
+
+// Expose OpenAPI Doc as Markdown for LLMs
+const markdown = await createMarkdownFromOpenApi(
+  JSON.stringify(
+    app.getOpenAPI31Document({
+      openapi: '3.1.0',
+      info: { title: 'Example', version: 'v1' },
+    })
+  )
+)
+app.get('/llms.txt', async (c) => c.text(markdown))
 
 export default {
   port: 3000,
